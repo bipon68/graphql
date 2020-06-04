28a17422471baa141737e207b57c6b895235631e
bipon68 <bipon68@users.noreply.github.com>
2020-06-04 20:42:40 +0600

    author list showing functionality

--- server.js
+++ server.js
@@ -28,13 +28,28 @@ const books = [
 	{ id: 8, name: 'Beyond the Shadows', authorId: 3 }
 ]
 
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    description: 'This represents a author of a book',
+    fields: () => ({
+      id: { type: GraphQLNonNull(GraphQLInt) },
+      name: { type: GraphQLNonNull(GraphQLString) }
+    })
+  })
+
 const BookType = new GraphQLObjectType({
     name: 'Book',
     description: 'This represents a book written by an author',
     fields: () => ({
       id: { type: GraphQLNonNull(GraphQLInt) },
       name: { type: GraphQLNonNull(GraphQLString) },
-      authorId: { type: GraphQLNonNull(GraphQLInt) }
+      authorId: { type: GraphQLNonNull(GraphQLInt) },
+      author: {
+        type: AuthorType,
+        resolve: (book) => {
+          return authors.find(author => author.id === book.authorId)
+        }
+      }
     })
   })
 
