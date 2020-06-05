ba0602e1330e5a717be47820c082e09ebe3bf480
bipon68 <bipon68@users.noreply.github.com>
2020-06-05 12:12:24 +0600

    continent showing in html using fetch api

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const continentSelect = document.getElementById('continent-select')
+const countryList = document.getElementById('countries-list')
+
+
 fetch('https://countries.trevorblades.com/', {
     method: 'POST',
     headers: { "Content-Type": "application/json" },
@@ -14,5 +18,11 @@ fetch('https://countries.trevorblades.com/', {
 })
 .then(res => res.json())
 .then(data => {
-    console.log(data.data)
+    console.log(data.data.continents)
+    data.data.continents.forEach(continent => {
+        const option = document.createElement('option')
+        option.value = continent.code
+        option.innerText = continent.name
+        continentSelect.appendChild(option)
+    })
 })
