ee7bf977fb54ea881d6bb13a81ac8f1d37400cbd
bipon68 <bipon68@users.noreply.github.com>
2020-06-05 12:02:49 +0600

    client with fetch in GraphQL

---
+++ script.js
@@ -0,0 +1,18 @@
+fetch('https://countries.trevorblades.com/', {
+    method: 'POST',
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+        query: `
+        query{
+            continents{
+              name,
+              code
+            }
+          }
+        `
+    })
+})
+.then(res => res.json())
+.then(data => {
+    console.log(data.data)
+})
