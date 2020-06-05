58656b25c4d9d4e99e1d71164629cea0b100601b
bipon68 <bipon68@users.noreply.github.com>
2020-06-05 12:37:53 +0600

    restructure query and added a function for feching

--- script.js
+++ script.js
@@ -2,27 +2,56 @@ const continentSelect = document.getElementById('continent-select')
 const countryList = document.getElementById('countries-list')
 
 
-fetch('https://countries.trevorblades.com/', {
-    method: 'POST',
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-        query: `
-        query{
-            continents{
-              name,
-              code
-            }
-          }
-        `
-    })
-})
-.then(res => res.json())
-.then(data => {
+queryFetch(`
+  query {
+    continents {
+      name
+      code
+    }
+  }
+`).then(data => {
     console.log(data.data.continents)
-    data.data.continents.forEach(continent => {
-        const option = document.createElement('option')
-        option.value = continent.code
-        option.innerText = continent.name
-        continentSelect.appendChild(option)
-    })
+  data.data.continents.forEach(continent => {
+    const option = document.createElement('option')
+    option.value = continent.code
+    option.innerText = continent.name
+    continentSelect.append(option)
+  })
 })
+
+function queryFetch(query) {
+    return fetch('https://countries.trevorblades.com/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        query: query
+      })
+    }).then(res => res.json())
+  }
+
+
+/**
+ * country qurry
+ */
+
+// query{
+//     continent(code:"AF"){
+//       countries{
+//         name
+//       }
+//     }
+//   }
+
+/**
+ * dynamic code
+ */
+
+// query{
+//     getCountries($code: Strign){
+//       continent(code:$code){
+//         countries{
+//         name
+//       }
+//       }
+//     }
+//   }
